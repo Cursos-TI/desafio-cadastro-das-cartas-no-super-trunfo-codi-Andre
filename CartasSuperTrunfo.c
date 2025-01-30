0875c0e2ae32dfa69b0878df9ca723e326917f14
André Miranda <120103103+codi-Andre@users.noreply.github.com>
2025-01-30 21:17:52 +0000

    :sparkles: feat(desafio): conclui o nível mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,9 +10,10 @@ int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     unsigned int populacao1, populacao2, area1, area2, pontosTuristicos1, pontosTuristicos2;
-    long unsigned int pib1, pib2;
+    long unsigned int pib1, pib2, superPoder1, superPoder2;
     char cidade1[255], cidade2[255];
     float densidadeDemografica1, densidadeDemografica2, pibPerCapita1, pibPerCapita2;
+    int pontuacao1 = 0, pontuacao2 = 0;
 
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
@@ -63,6 +64,8 @@ int main() {
     densidadeDemografica2 = (float) pib2 / populacao2;
     pibPerCapita1 = (float) pib1 / populacao1;
     pibPerCapita2 = (float) populacao2 / area2;
+    superPoder1 = populacao1 + area1 + pib1 + pontosTuristicos1 + densidadeDemografica1 + pibPerCapita1;
+    superPoder2 = populacao2 + area2 + pib2 + pontosTuristicos2 + densidadeDemografica2 + pibPerCapita2;
 
     
     // Exibição dos Dados das Cartas:
@@ -85,6 +88,37 @@ int main() {
     printf("PIB: R$ %lu,00\n", pontosTuristicos2);
     printf("Densidade demográfica: %.2f hab/km²\n", densidadeDemografica2);
     printf("PIB per Capita: R$ %.2f \n\n\n", pibPerCapita2);
+
+    // Duelo
+    
+    printf("Duelo de cidades\n");
+    printf("Cidade %s vs cidade %s\n\n", cidade1, cidade2);
+
+    printf("População:             %d vs %d\n", populacao1 > populacao2, populacao1 < populacao2);
+    printf("Área:                  %d vs %d\n", area1 > area2, area1 < area2);
+    printf("Pontos turísticos:     %d vs %d\n", pontosTuristicos1 > pontosTuristicos2, pontosTuristicos1 < pontosTuristicos2);
+    printf("PIB:                   %d vs %d\n", pib1 > pib2, pib1 < pib2);
+    printf("Densidade demográfica: %d vs %d\n", densidadeDemografica1 < densidadeDemografica2, densidadeDemografica1 > densidadeDemografica2);
+    printf("PIB per Capita:        %d vs %d\n", pibPerCapita1 > pibPerCapita2, pibPerCapita1 < pibPerCapita2);
+    printf("Super poder:           %d vs %d\n\n", superPoder1 > superPoder2, superPoder1 < superPoder2);
+
+    pontuacao1 += populacao1 > populacao2;
+    pontuacao1 += area1 > area2;
+    pontuacao1 += pontosTuristicos1 > pontosTuristicos2;
+    pontuacao1 += pib1 > pib2;
+    pontuacao1 += densidadeDemografica1 < densidadeDemografica2;
+    pontuacao1 += pibPerCapita1 > pibPerCapita2;
+    pontuacao1 += superPoder1 > superPoder2;
+
+    pontuacao2 += populacao1 < populacao2;
+    pontuacao2 += area1 < area2;
+    pontuacao2 += pontosTuristicos1 < pontosTuristicos2;
+    pontuacao2 += pib1 < pib2;
+    pontuacao2 += densidadeDemografica1 > densidadeDemografica2;
+    pontuacao2 += pibPerCapita1 < pibPerCapita2;
+    pontuacao2 += superPoder1 < superPoder2;
+
+    printf("Pontuação final: Cidade %s com %d pontos e cidade %s com %d pontos\n\n\n", cidade1, pontuacao1, cidade2, pontuacao2);
 
     return 0;
 }
